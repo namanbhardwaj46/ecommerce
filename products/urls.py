bc7b24a6f42ff87420e47f62538d1211f156c7ac
Naman Bhardwaj <namanbhardwaj46@gmail.com>
2025-04-25 11:44:16 +0530

    testing

--- products/urls.py
+++ products/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # Maps the 'hello' view, which could be an introductory or test endpoint
     path('hello/', views.hello),
     # Maps the 'create_or_get_products' view, which returns all products or can create new products and save.
-    path('Products/', views.create_or_get_products),
+    path('products/', views.create_or_get_products),
     # Maps the 'get_product' view, which returns a single product by ID.
     path('product/<int:id>/', views.get_product),
     # Maps the 'filter_product' view, which filters products based on query parameters.
